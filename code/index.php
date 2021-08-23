643d137fb1e39a00efb5d3ce467afd8d7d7889f2
Manish <76569486+kr123Manish@users.noreply.github.com>
2021-08-23 12:10:41 +0530

    Update index.php

--- code/index.php
+++ code/index.php
@@ -4,7 +4,7 @@ $merror="";
 $nerror="";
 $data="";
 if (array_key_exists("login", $_POST)) {
-   $link=mysqli_connect("sdb-e.hosting.stackcp.net","flight-3138337e65","Manish123","flight-3138337e65");
+   $link=mysqli_connect("sdb-e.hosting.stagckcpp.net","flight-313833rt7e65","Password","flight-313833rt7e65");
    if (mysqli_connect_error()) {
       // echo "There was an error to connecting to the database";
       die("Database Connection Error");
